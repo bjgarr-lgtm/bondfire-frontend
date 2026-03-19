f6a0a85298f144808883de38376a91d1a233c681
bjgarr-lgtm <bjgarr@gmail.com>
2026-03-19 00:18:20 -0700

    sfs

--- src/components/drive/NotePreview.jsx
+++ src/components/drive/NotePreview.jsx
@@ -7,10 +7,14 @@ function simpleMarkdown(md) {
     .replace(/^# (.*$)/gim, "<h1>$1</h1>")
     .replace(/\*\*(.*?)\*\*/gim, "<strong>$1</strong>")
     .replace(/\*(.*?)\*/gim, "<em>$1</em>")
-    .replace(/\n/gim, "<br />")}
+    .replace(/\n/gim, "<br />");
+}
 
 export default function NotePreview({ content }) {
   return (
-    <div style={{ border: "1px solid #333", padding: 8, minHeight: "70vh", overflow: "auto" }} dangerouslySetInnerHTML={{ __html: simpleMarkdown(content) }} />
+    <div
+      style={{ border: "1px solid #333", padding: 8, minHeight: "70vh", overflow: "auto" }}
+      dangerouslySetInnerHTML={{ __html: simpleMarkdown(content) }}
+    />
   );
 }
